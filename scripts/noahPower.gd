a1cabfe3f9e6ee966bcb7f66654a56e14421c3b9
marcfreir <marcfreir@outlook.com>
2021-10-30 02:13:53 -0200

    Update & limit the power of the main character

--- scripts/noahPower.gd
+++ scripts/noahPower.gd
@@ -7,9 +7,13 @@ var powerDirection = Vector2(0,-1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_to_group("noahPower")
 	set_process(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translate(powerDirection * powerSpeed * delta)
+	
+	if get_global_position().y < 0:
+		queue_free()
