ff7a24dbb90d96c0fbbc34ea27f6e768ed935d47
marcfreir <marcfreir@outlook.com>
2021-10-31 03:35:21 -0200

    Set monster power

---
+++ scripts/monsterPower.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const speed = 120
+const direction = Vector2(0,1)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(direction * speed * delta)
+	
+func destroy(object):
+	queue_free()
